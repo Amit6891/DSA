dc12795d18e8193ce3ad14f70537b48c47a6afbd
Amit Chauhan <amit.chauhan.6891@outlook.com>
2024-10-21 06:50:30 +0530

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,115 @@
+﻿namespace DSA
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void AddFirst(T data)
+        {
+            var newNode = new Node<T>(data);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void AddLast(T data)
+        {
+            var newNode = new Node<T>(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                var currentNode = Head;
+                while(currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+        }
+
+        public Node<T> Find(T data)
+        {
+            var currentNode = Head;
+            while(currentNode != null)
+            {
+                if (currentNode.Data.Equals(data))
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.Next;
+            }
+            return null;
+        }
+
+        public int Size()
+        {
+            var count = 0;
+            var currentNode = Head;
+            while(currentNode != null)
+            {
+                count++;
+                currentNode = currentNode.Next;
+            }
+            return count;
+        }
+
+        public void PrintList()
+        {
+            var currentNode = Head;
+            while(currentNode != null)
+            {
+                Console.Write(currentNode.Data + " ");
+                currentNode = currentNode.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public void RemoveFirst()
+        {
+            Head = Head.Next;
+        }
+
+        public void Remove(T data)
+        {
+            if (Head.Data.Equals(data))
+            {
+                Head = Head.Next;
+                return;
+            }
+            var currentNode = Head;
+            while(currentNode.Next != null)
+            {
+                if (currentNode.Next.Data.Equals(data))
+                {
+                    currentNode.Next = currentNode.Next.Next;
+                    return;
+                }
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace DSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays
+
+            Console.WriteLine("--------------Arrays----------");
+
+            var intArray = new int[] { 4, 7, 9, 1, 2, 3, 4, 5 };
+            int[] intArray2 = { 6, 7, 8, 9, 2, 5, 6 };
+            Console.WriteLine("Print Array");
+            foreach (var item in intArray)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Array.Sort(intArray);
+            Console.WriteLine();
+            Console.WriteLine("Sorted Array");
+            foreach (var item in intArray)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Array.Reverse(intArray);
+            Console.WriteLine();
+            Console.WriteLine("Reverse Sort Array");
+            foreach (var item in intArray)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("INdex of number 4");
+            Console.WriteLine(Array.IndexOf(intArray, 4));
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region Linked List
+
+            Console.WriteLine("--------------Linked List----------");
+
+            // Create a linked list
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+
+            list.PrintList();  // Output: 10 -> 20 -> 30 -> null
+
+            list.AddFirst(5);
+            list.PrintList();  // Output: 5 -> 10 -> 20 -> 30 -> null
+
+            list.Remove(20);
+            list.PrintList();  // Output: 5 -> 10 -> 30 -> null
+
+            Node<int> foundNode = list.Find(10);
+            Console.WriteLine(foundNode != null ? "Found" : "Not Found");  // Output: Found
+
+            Console.WriteLine("Size: " + list.Size());  // Output: Size: 3
+            Console.WriteLine("Is Empty: " + list.IsEmpty());  // Output: Is Empty: False
+
+            #endregion
+        }
+    }
+}
